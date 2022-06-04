c5c89a2ccc4578318d9b16db8e0de3b8c36c47f5
hodu42 <knn330@naver.com>
2022-06-04 22:30:46 +0900

    완성?

--- BankAppDemoGUI/src/main/java/com/gui/UsersFileManager.java
+++ BankAppDemoGUI/src/main/java/com/gui/UsersFileManager.java
@@ -7,44 +7,41 @@ public class UsersFileManager {
     private static String filePath = "users";
 
     public static void addUser(User user) throws IOException, ClassNotFoundException {
-        File accFile = new File(filePath);
-        FileInputStream fis = new FileInputStream(accFile);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-        TreeMap<String, User> users = (TreeMap<String, User>) ois.readObject();
+        TreeMap<String, User> users = getTreeMap(new File(filePath));
 
         users.put(user.getId(), user);
 
-        FileOutputStream fos = new FileOutputStream(accFile);
-        ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-        oos.writeObject(users);
-        oos.flush();
-        oos.close();
+        writeFile(new File(filePath), users);
     }
 
     public static void saveUser(User user) throws IOException, ClassNotFoundException {
-        File accFile = new File(filePath);
-        FileInputStream fis = new FileInputStream(accFile);
-        ObjectInputStream ois = new ObjectInputStream(fis);
 
-        TreeMap<String, User> users = (TreeMap<String, User>) ois.readObject();
+        TreeMap<String, User> users = getTreeMap(new File(filePath));
         users.replace(user.getId(), user);
 
+        writeFile(new File(filePath), users);
+    }
+
+    public static User findUser(String id) throws IOException, ClassNotFoundException {
+        TreeMap<String, User> users = getTreeMap(new File(filePath));
+
+        return users.get(id);
+    }
+
+    private static TreeMap getTreeMap(File accFile) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(accFile);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        TreeMap<String, User> users = (TreeMap<String, User>) ois.readObject();
+
+        return users;
+    }
+
+    private static void writeFile(File accFile, TreeMap users) throws IOException {
         FileOutputStream fos = new FileOutputStream(accFile);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
 
         oos.writeObject(users);
         oos.flush();
         oos.close();
-    }
-
-    public static User findUser(String id) throws IOException, ClassNotFoundException {
-        File accFile = new File(filePath);
-        FileInputStream fis = new FileInputStream(accFile);
-        ObjectInputStream ois = new ObjectInputStream(fis);
-
-        TreeMap<String, User> users = (TreeMap<String, User>) ois.readObject();
-
-        return users.get(id);
     }
 }
