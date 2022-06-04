246609e9ee777eccdcdeac2e9cc8eccee0944c6d
hodu42 <knn330@naver.com>
2022-06-04 16:25:56 +0900

    IAccount 만듬, Account 상속받게 수정

---
+++ BankAppDemoGUI/src/main/java/com/gui/IAccount.java
@@ -0,0 +1,18 @@
+package com.gui;
+
+public interface IAccount {
+
+    public int getAccountNumber();
+
+    public int getBalance();
+
+    public void deposit(int amount);
+
+    public void withdraw(int amount);
+
+    public void transfer(int accNum, int amount);
+
+    public boolean validatePIN(int pin);
+
+    public String toString();
+}
